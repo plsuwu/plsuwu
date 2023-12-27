46db96e752c0805fcd797a32b91fb8cca6cb7ac0
pls <zac.womal@outlook.com>
2023-12-28 03:01:24 +1000

    Delete build directory

--- build/server/chunks/4-fOp05PJu.js
+++
@@ -1,9 +0,0 @@
-const index = 4;
-let component_cache;
-const component = async () => component_cache ??= (await import('./_page.svelte-i2GDyjqe.js')).default;
-const imports = ["_app/immutable/nodes/4.nM2xStGb.js","_app/immutable/chunks/scheduler.rijuCTql.js","_app/immutable/chunks/index.7JfBVtF2.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=4-fOp05PJu.js.map
--- build/server/chunks/slugFromPath-TkjAgDnQ.js
+++
@@ -1,4 +0,0 @@
-const slugFromPath = (path) => path.match(/([\w-]+)\.(svelte\.md|md|svx)/i)?.[1] ?? null;
-
-export { slugFromPath as s };
-//# sourceMappingURL=slugFromPath-TkjAgDnQ.js.map
