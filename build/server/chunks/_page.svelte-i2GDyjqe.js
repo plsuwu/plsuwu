125ed0adc762b8fef18c1b3490998482dfe3a525
pls <zac.womal@outlook.com>
2023-12-28 04:04:56 +1000

    Delete build directory
    `npm ci`

--- build/server/chunks/_page.svelte-i2GDyjqe.js
+++
@@ -1,8 +0,0 @@
-import { c as create_ssr_component } from './ssr-3xKC9Uba.js';
-
-const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<div class="mt-28 lg:mt-0 w-full flex flex-col" data-svelte-h="svelte-tl6ym"><div class="self-center shadow-inner w-full bg-cat-mantle py-10"><div class="text-cat-text text-center font-bold my-10"><div class="text-6xl lg:text-3xl text-center my-8 font-bold text-cat-overlay0">notes</div></div> <div class="mt-12 text-cat-text min-w-3xl max-w-6xl rounded-xl mx-auto bg-cat-crust"><div class="text-3xl lg:text-xl p-12 space-y-8 mx-8 lg:mx-36 text-justify"><p>No content yet :(</p></div></div></div></div>`;
-});
-
-export { Page as default };
-//# sourceMappingURL=_page.svelte-i2GDyjqe.js.map
