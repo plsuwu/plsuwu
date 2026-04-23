ad3d7830125578a9eaa968c85658300adb49bf3f
plsuwu <124419933+plsuwu@users.noreply.github.com>
2026-04-23 20:39:29 +1000

    actual initial commit

--- src/components/landing/Banner.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-</script>
-
-<div class="flex w-full flex-col items-center justify-center">
-	<div class="font-jacquard text-8xl">please</div>
-	<div class="mt-6 w-1/3 border border-b"></div>
-</div>
--- src/components/landing/index.ts
+++
@@ -1,3 +0,0 @@
-import Banner from "components/landing/Banner.svelte";
-
-export { Banner };
