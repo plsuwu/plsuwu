17c2da817bc267e5f7f30359885e1b62eca8763e
plsuwu <124419933+plsuwu@users.noreply.github.com>
2026-07-28 21:54:08 +1000

    initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            bun
+            astro-language-server
+          ];
+        };
+      }
+    );
+}
